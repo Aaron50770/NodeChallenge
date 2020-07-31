8f4a61c4b2ada77b812b164989ecd6a6bd7500c3
TXT-44 <txt-44@MacBook-Pro-8.local>
2020-07-30 22:36:49 -0700

    This is the node challenge

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+   
+const app = express();
+
+const port = 3000;
+
+app.listen (port, function(){
+    console.log ('listening to port', port);
+})
+
+app.get('/home', function(req, res){
+    res.sendFile('./home.html', {root: __dirname})
+})
+
+app.get('/about', function(req, res){
+    res.sendFile('./about.html', {root: __dirname})
+})
+
+app.get('/contact', function(req, res){
+    res.sendFile('./contact.html', {root: __dirname})
+})
+
+app.get('/', function(req, res){
+    res.sendStatus(401)
+})
+    
